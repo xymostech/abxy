8a7fbed56253b3848d8e1c10821c724365ff0fe8
Emily Eisenberg <xymostech@gmail.com>
2012-11-14 14:29:44 -0500

    Fix weird Entity2d spacing

--- gamelib/Entity2d.hpp
+++ gamelib/Entity2d.hpp
@@ -19,27 +19,19 @@ public:
 	virtual ~Entity2d();
 
 	virtual const Vector2 &Position() const;
-
 	virtual const Vector2 &Velocity() const;
-
 	virtual const float &Angle() const;
-
 	virtual const float &AngVelocity() const;
 
 	virtual Entity2d &SetPosition(const Vector2 &new_position);
-
 	virtual Entity2d &Shift(const Vector2 &offset);
-
 	virtual Entity2d &SetVelocity(const Vector2 &new_velocity);
 
 	virtual Entity2d &SetAngle(const float &new_angle);
-
 	virtual Entity2d &turn(const float &tilt);
-
 	virtual Entity2d &SetAngVelocity(const float &new_angvelocity);
 
 	virtual void Update();
-
 	virtual void Draw(Matrix4 model_matrix) const;
 };
 
