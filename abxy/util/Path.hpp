d0465315ee6f64e79900affa4c3c54dfe42928bd
Emily Eisenberg <xymostech@gmail.com>
2013-02-15 22:42:44 -0500

    Add some comments

--- abxy/util/Path.hpp
+++ abxy/util/Path.hpp
@@ -10,8 +10,13 @@
 
 namespace Util {
 
+	/* Gets the path of the currently running executable */
 	std::string GetExecutablePath();
+
+	/* Gets the current working directory */
 	std::string GetCWD();
+
+	/* Sets the current working directory. Returns false on failure */
 	bool SetCWD(std::string path);
 
 }
