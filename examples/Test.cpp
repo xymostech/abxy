10fd481d7df19eca4f8feae83f38c0b7c09d256d
Emily Eisenberg <xymostech@gmail.com>
2013-03-02 21:24:04 -0500

    Make the example do something

--- examples/Test.cpp
+++ examples/Test.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+
+#include <abxy/Game.hpp>
+#include <abxy/entity/WorldEntity.hpp>
+#include <abxy/entity/Entity3d.hpp>
+
 int main() {
-	/* code */
+	Game test_game(60);
+	test_game.Startup();
+
+	while (test_game.IsOpen()) {
+		test_game.Update();
+
+		test_game.Draw();
+	}
+
 	return 0;
 }
