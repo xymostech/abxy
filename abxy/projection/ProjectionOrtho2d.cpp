dc319db7365fb3e408785d49be766c3c2637a7da
Emily Eisenberg <xymostech@gmail.com>
2013-03-15 12:51:44 -0400

    Make the ortho2d projection more reliable
    Don't have it do any scaling, just have it make sure that the y/x ratio
    is the same as the screen.

--- abxy/projection/ProjectionOrtho2d.cpp
+++ abxy/projection/ProjectionOrtho2d.cpp
@@ -6,12 +6,16 @@ ProjectionOrtho2d::ProjectionOrtho2d()
 {
 	SetCameraToClipMatrix(camera_clip_matrix);
 	SetWorldToCameraMatrix(world_camera_matrix);
+
+	Resize();
 }
 
 void ProjectionOrtho2d::Resize() {
 	glViewport(0, 0, window_width, window_height);
-	camera_clip_matrix[0][0] = 2.0 / (float) window_width * 10;
-	camera_clip_matrix[1][1] = 2.0 / (float) window_height * 10;
+
+	float max = std::max(window_width, window_height);
+	camera_clip_matrix[0][0] = max / (float) window_width;
+	camera_clip_matrix[1][1] = max / (float) window_height;
 	SetCameraToClipMatrix(camera_clip_matrix);
 }
 
